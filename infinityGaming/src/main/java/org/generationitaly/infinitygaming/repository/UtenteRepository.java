3017cb87b16bbb2708b6ee7b346046e7a57e05c2
Dart1998-tech <cianchettidaniel90@gmail.com>
2025-03-28 16:59:55 +0100

    Delete infinityGaming directory

--- infinityGaming/src/main/java/org/generationitaly/infinitygaming/repository/UtenteRepository.java
+++
@@ -1,9 +0,0 @@
-package org.generationitaly.infinitygaming.repository;
-
-import org.generationitaly.infinitygaming.entity.Utente;
-
-public interface UtenteRepository extends JpaRepository<Utente, Long> {
-
-	Utente findByUsername(String username);
-
-}
